c6ab541d7a2036bdcbd8b041c6ba2fc11b091340
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2025-10-04 18:45:57 +0200

    add nvim config

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.relativenumber = true
+vim.opt.backupcopy = "yes"
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 8
---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/efaz/.config/omarchy/current/theme/neovim.lua
