257a0a66d55636464e2806e6598242b685b3ec89
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2025-10-08 17:27:41 +0200

    add simonw/llm

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -18,3 +18,10 @@ alias cr='cd ~'
 alias co='cd ~/Downloads'
 
 alias unzip='tar -xzf'
+
+# 1. llm --system $"You are an expert on Arch linux, ghostty, command line, TUI, zsh, etc. Respond as a power user. Hard cap: 10-11 lines. Nice formatting, to the point, commands in code syntax when useful; no fluff." --save ask
+# 2. llm templates edit ask [for adding the model name]
+
+ask() {
+  command llm -t ask "$@" | glow
+}
