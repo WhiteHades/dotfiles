c12d309587acc17825ba48a9e50e9bbace5ced09
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2026-01-18 09:14:57 +0600

    add yazi alias

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -102,3 +102,10 @@ aria() {
     "$url"
 }
 
+y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
