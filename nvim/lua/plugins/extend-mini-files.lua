65d7777d4d49b66f5718b986cbedadbb136fd233
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2026-01-20 06:47:49 +0600

    update nvim keybindings

---
+++ nvim/lua/plugins/extend-mini-files.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-mini/mini.files",
+  keys = {
+    {
+      "<leader>fm",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (Directory of Current File)",
+    },
+    {
+      "<leader>fM",
+      function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
+    },
+    {
+      "<leader>fd",
+      function()
+        require("mini.files").open(LazyVim.root(), true)
+      end,
+      desc = "Open mini.files (root)",
+    },
+  },
+}
